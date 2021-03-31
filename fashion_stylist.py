eb635c25b44a12cd82322228be4a7c17343e8065
Veronica Johnson <61217832+vdyej@users.noreply.github.com>
2021-03-31 14:26:27 -0400

    added # of days
     (but will require casting input string to integer)

--- fashion_stylist.py
+++ fashion_stylist.py
@@ -4,8 +4,14 @@ top = ["halter top", "polo shirt", "tank top", "basketball jersey", "fitted bodi
 bottom = ["bell-bottoms", "capri pants", "mom jeans", "cargo shorts", "a tutu", "glittery leggings", "acid-washed jeans", "a pencil skirt", "a ball gown", "a kilt", "sailor pants", "a boho skirt"]
 accessory = ["nose ring", "scrunchie", "set of bangles", "slap bracelet", "spiked collar", "pair of diamond earrings", "tiara", "fake tattoo", "top hat", "beret", "newsboy cap", "choker", "fake beauty mark"]
 footwear = ["Doc Martens", "Birkenstocks", "flip-flops", "Mary Janes", "stilettos", "skater shoes", "Keds", "platform shoes", "cowboy boots", "gladiator sandals", "high heels", "Air Jordans"]
+compliment = ["Stunning!", "Gorgeous!", "Too cool!", "Breathtaking!", "Phenomenal!", "Cover-worthy!", "Unbelievably chic!", "Striking!", "Daring!"]
 
 name = input("Hi. What's your name? ")
+days = int(input("How many days of outfits should I plan for you?"))
 
-print ("You know what would look fab on you, " + name + "?! A " + random.choice(top) + " over " + random.choice(bottom) 
-+ "! Add a " + random.choice(accessory) + ", throw on a pair of " + random.choice(footwear) + ", and you'll be all set!")
+print("Here's a schedule of outfits that'll look fab on you, " + name + "! ")
+
+for x in range(1,days): 
+    print("Day " + str(x) + ":  A " + random.choice(top) + " over " + random.choice(bottom) 
+    + "! Add a " + random.choice(accessory) + ", throw on a pair of " + random.choice(footwear) + ". " + random.choice(compliment))
+
