a7618522cd9d74dd6b86291e586f68e94c087e48
Veronica Johnson <61217832+vdyej@users.noreply.github.com>
2021-04-01 11:13:58 -0400

    base code - no frills

---
+++ sorting_hat.py
@@ -0,0 +1,8 @@
+import random
+
+houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+name = input("What's your name?")
+
+print(name + "..." + "hmmm...." + "you are indubitably of the House of " + random.choice(houses))
+
