fb311c706b008b07ed66a9f4d21eb3417cbeca30
Veronica Johnson <61217832+vdyej@users.noreply.github.com>
2021-04-01 11:28:45 -0400

    quiz with if conditional added
    descriptions of houses found here https://online.ucpress.edu/collabra/article/5/1/31/113037/The-Science-Behind-the-Magic-The-Relation-of-the

--- sorting_hat.py
+++ sorting_hat.py
@@ -2,7 +2,20 @@ import random
 
 houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
-name = input("What's your name?")
+name = input("What's your name? ")
 
-print(name + "..." + "hmmm...." + "you are indubitably of the House of " + random.choice(houses))
+answer = input("Which of the following best describes you? A) Brave & Helpful   B) Patient & Loyal   C) Intellligent & Witty   D) Ambitious & Cunning :")
+
+if answer == "A":
+    match = "Gryffindor"
+elif answer == "B":
+    match = "Hufflepuff"
+elif answer == "C":
+    match = "Ravenclaw"
+elif answer == "D":
+    match = "Slytherin"
+else: 
+    match = random.choice(houses)
+
+print(name + "..." + "hmmm...." + "you are indubitably of the House of " + match)
 
